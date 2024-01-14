dffd2a642fd9a6a979ca2fcad57560871fd5e667
Makadoro <tim.koehler86@googlemail.com>
2024-01-14 11:33:50 +0100

    Initial commit
    Changes to be committed:
    	new file:   .gitignore
    	new file:   .vscode/launch_sample.json
    	new file:   .vscode/tasks.json
    	new file:   dist/rivet.js
    	new file:   dist/rivet.js.map
    	new file:   example.rivet-project
    	new file:   package-lock.json
    	new file:   package.json
    	new file:   readme.md
    	new file:   src/rivet.ts
    	new file:   tsconfig.json

---
+++ src/rivet.ts
@@ -0,0 +1,27 @@
+import { runGraphInFile, startDebuggerServer, NodeDatasetProvider } from '@ironclad/rivet-node';
+// Start debugger server
+const debuggerServer = await startDebuggerServer({});
+
+let project = "./example.rivet-project";
+let graph = "example-graph";
+
+// Get OPEN_API_KEY from environment
+let openAiKey = process.env.OPEN_API_KEY;
+
+let result = await runGraphInFile(project, {
+    graph: graph,
+    remoteDebugger: debuggerServer,
+    inputs: {
+      "prompt": "Please write me a short poem about a dog."
+    },
+    context: {},
+    externalFunctions: {},
+    onUserEvent: {},
+    openAiKey: openAiKey
+  });
+
+// Return graph output of the run
+console.log(result.response.value);
+
+// Force the application to exit (otherwise it keeps running as debugger still runs and does not have a close function)
+process.exit();
